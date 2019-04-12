296f61e588c5819443731dff80991f750dd6d51b
mingzhenliu <mingzhenliu@webank.com>
2019-04-12 10:48:49 +0800

    init webase-sign

---
+++ src/main/java/com/webank/webase/sign/base/BaseController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.webank.webase.sign.base;
+ 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.webank.webase.sign.base.exception.BaseException;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * BaseController.
+ * 
+ */
+@Slf4j
+public abstract class BaseController {
+    /**
+     * checkParamResult.
+     * 
+     * @param bindingResult checkResult
+     */
+    protected BaseResponse checkParamResult(BindingResult bindingResult) throws BaseException {
+        if (!bindingResult.hasErrors()) {
+            return null;
+        }
+
+        String errorMsg = getParamValidFaildMessage(bindingResult);
+        if (StringUtils.isBlank(errorMsg)) {
+            log.warn("OnWarning:param exception. errorMsg is empty");
+            throw new BaseException(ConstantCode.PARAM_VAILD_FAIL);
+        }
+
+        RetCode retCode = null;
+        try {
+            JSONObject jsonObject = JSON.parseObject(errorMsg);
+            retCode = JSONObject.toJavaObject(jsonObject, RetCode.class);
+        } catch (Exception ex) {
+            log.warn("OnWarning:retCodeJson convert error");
+            throw new BaseException(ConstantCode.PARAM_VAILD_FAIL);
+        }
+
+        throw new BaseException(retCode);
+    }
+
+    private String getParamValidFaildMessage(BindingResult bindingResult) {
+        List<ObjectError> errorList = bindingResult.getAllErrors();
+        log.info("errorList:{}", JSON.toJSONString(errorList));
+        if (errorList == null) {
+            log.warn("onWarning:errorList is empty!");
+            return null;
+        }
+
+        ObjectError objectError = errorList.get(0);
+        if (objectError == null) {
+            log.warn("onWarning:objectError is empty!");
+            return null;
+        }
+        return objectError.getDefaultMessage();
+    }
+}
---
+++ src/main/java/com/webank/webase/sign/keystore/KeyStoreService.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.webank.webase.sign.keystore;
+
+import org.fisco.bcos.web3j.crypto.ECKeyPair;
+import org.fisco.bcos.web3j.crypto.Keys;
+import org.fisco.bcos.web3j.utils.Numeric;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.webank.webase.sign.base.ConstantCode;
+import com.webank.webase.sign.base.exception.BaseException;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * KeyStoreService.
+ *
+ */
+@Slf4j
+@Service
+public class KeyStoreService {
+    static final int PUBLIC_KEY_LENGTH_IN_HEX = 128;
+
+    /**
+     * getKey.
+     * 
+     * @return
+     * @throws BaseException 
+     */
+    public KeyStoreInfo getKey() throws BaseException {
+        try {
+            ECKeyPair keyPair = Keys.createEcKeyPair();
+            String publicKey = Numeric.toHexStringWithPrefixZeroPadded(keyPair.getPublicKey(),
+                    PUBLIC_KEY_LENGTH_IN_HEX);
+            String privateKey = Numeric.toHexStringNoPrefix(keyPair.getPrivateKey());
+            String address = "0x" + Keys.getAddress(publicKey);
+
+            KeyStoreInfo keyStoreInfo = new KeyStoreInfo();
+            keyStoreInfo.setPublicKey(publicKey);
+            keyStoreInfo.setPrivateKey(privateKey);
+            keyStoreInfo.setAddress(address);
+
+            log.info("getKey finish. keyStoreInfo[{}]", JSON.toJSONString(keyStoreInfo));
+            return keyStoreInfo;
+        } catch (Exception e) {
+            log.error("createEcKeyPair fail.");
+            throw new BaseException(ConstantCode.SYSTEM_ERROR);
+        }
+    }
+}
