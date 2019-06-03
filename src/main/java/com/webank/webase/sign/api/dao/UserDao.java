a7409d8c83611a034302b4b8ad6c999b8b52e9e0
dwusiq <dwusiq@163.com>
2019-06-03 10:47:16 +0800

    update logAspect

--- src/main/java/com/webank/webase/sign/api/dao/UserDao.java
+++ src/main/java/com/webank/webase/sign/api/dao/UserDao.java
@@ -24,7 +24,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDao {
 
-    void createTable();
+    void createTbUser();
 
     void insertUserInfo(UserInfoPo userInfoPo);
 
--- src/main/java/com/webank/webase/sign/config/TableInitConfig.java
+++ src/main/java/com/webank/webase/sign/config/TableInitConfig.java
@@ -29,6 +29,6 @@ public class TableInitConfig implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
-        userDao.createTable();
+        userDao.createTbUser();
     }
 }
