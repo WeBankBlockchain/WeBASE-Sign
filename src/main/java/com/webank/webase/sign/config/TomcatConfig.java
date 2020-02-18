c70eae585536b5f6ae39d372af8f209c202852ea
yanyanho <yanyanho@126.com>
2020-02-18 23:02:08 +0800

    fix the keepAliveRequests configable

--- src/main/java/com/webank/webase/sign/config/TomcatConfig.java
+++ src/main/java/com/webank/webase/sign/config/TomcatConfig.java
@@ -1,6 +1,9 @@
 package com.webank.webase.sign.config;
 
+import com.webank.webase.sign.constant.ConstantProperties;
+import org.apache.coyote.Constants;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfig {
 
+
+    @Autowired
+    ConstantProperties constantProperties;
     @Bean
     public EmbeddedServletContainerFactory createEmbeddedServletContainerFactory() {
         TomcatEmbeddedServletContainerFactory tomcatFactory = new TomcatEmbeddedServletContainerFactory();
         tomcatFactory.addConnectorCustomizers(connector -> {
             Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-            //设置最大连接数
             protocol.setKeepAliveTimeout(10 * 1000);
-            protocol.setMaxKeepAliveRequests(1000);
+            protocol.setMaxKeepAliveRequests(constantProperties.getKeepAliveRequests());
         });
         return tomcatFactory;
     }
