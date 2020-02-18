83e7047570854c0390b0b9116319a9ac2348f2f0
yanyanho <yanyanho@126.com>
2020-02-18 22:54:55 +0800

    refactor the code

--- src/main/java/com/webank/webase/sign/Application.java
+++ src/main/java/com/webank/webase/sign/Application.java
@@ -34,7 +34,6 @@ public class Application {
 
     public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
-
         log.info("main run success...");
     }
 }
