907f154d5ad56c196d6479c7f301c275b1a60adb
CodingCattwo <847701726@qq.com>
2020-03-26 12:29:03 +0800

    unused import

--- src/main/java/com/webank/webase/sign/api/controller/UserController.java
+++ src/main/java/com/webank/webase/sign/api/controller/UserController.java
@@ -30,8 +30,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Optional;
 
