f5cda5a02df8566f995b4aa457f1bd0ae7965f74
marsli <marsli@webank.com>
2020-04-29 10:18:00 +0800

    add status of user

--- src/main/java/com/webank/webase/sign/pojo/vo/RspUserInfoVo.java
+++ src/main/java/com/webank/webase/sign/pojo/vo/RspUserInfoVo.java
@@ -23,12 +23,13 @@ import lombok.Data;
  */
 @Data
 public class RspUserInfoVo {
-	private String signUserId;
-	private String appId;
-	private String address;
-	private String publicKey;
-	// not return privateKey
+    private String signUserId;
+    private String appId;
+    private String address;
+    private String publicKey;
+    // not return privateKey
     private String privateKey;
     private String description;
     private Integer encryptType;
+    private String status;
 }
