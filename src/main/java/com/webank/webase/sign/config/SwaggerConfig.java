7a2246cba05fe791a680a5f05bc9a59a9a07f7f5
CodingCattwo <847701726@qq.com>
2022-05-17 15:56:26 +0800

    fix swagger

--- src/main/java/com/webank/webase/sign/config/SwaggerConfig.java
+++ src/main/java/com/webank/webase/sign/config/SwaggerConfig.java
@@ -19,6 +19,7 @@ import com.google.common.collect.Lists;
 import java.util.HashSet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -27,7 +28,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
     /**
      * documentation.
