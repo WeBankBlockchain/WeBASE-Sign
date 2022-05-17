5be5ff2b248d8065875044b7d747f70c571301b8
CodingCattwo <847701726@qq.com>
2022-05-17 16:10:00 +0800

    fix swagger

--- src/main/java/com/webank/webase/sign/config/SwaggerConfig.java
+++ src/main/java/com/webank/webase/sign/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
     /**
      * documentation.
